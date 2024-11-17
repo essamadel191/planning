54dbedd4d4d3da3657fb3f38a13335a17a1a7bbc
essam191 <essam.adel191@gmail.com>
2024-11-17 22:07:23 +0200

    set up entity framework

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07738b3593e83ce60e6c8a0921865d3173591872")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872d6f6c166a0297d435a8425d179d664654a65e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
